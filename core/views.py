6f6bee90d2498304c464558734d5abf739a30716
Keerty Verma <keertyverma1303@gmail.com>
2020-05-17 01:53:20 +0530

    Support for name search and is active filter

--- core/views.py
+++ core/views.py
@@ -5,6 +5,8 @@ from django.urls import reverse_lazy
 
 from datetime import datetime
 
+import django_filters
+
 from .forms import *
 from .models import *
 
@@ -13,10 +15,26 @@ class Home(TemplateView):
     template_name = 'home.html'
 
 
+class ProductFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(
+        field_name='name', lookup_expr='icontains')
+    is_active = django_filters.BooleanFilter(field_name='is_active')
+
+    class Meta:
+        model = ProductModel
+        fields = ('name', 'is_active')
+
+
 class ProductListView(ListView):
     model = ProductModel
     template_name = 'products/list.html'
     context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = ProductFilter(
+            self.request.GET, queryset=self.get_queryset())
+        return context
 
 
 class CreateProductView(CreateView):
--- settings/base.py
+++ settings/base.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'crispy_forms',
+    'django_filters',
+    'bootstrapform',
 
     'core'
 ]
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
