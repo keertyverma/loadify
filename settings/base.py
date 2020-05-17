ad8b91cd16c76e15afc9fdf31c7757d29d33e4ee
Keerty Verma <keertyverma1303@gmail.com>
2020-05-18 00:13:32 +0530

    serve static files to heroku via whitenoise

--- settings/base.py
+++ settings/base.py
@@ -134,8 +134,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MEDIA_ROOT = 'media/'
@@ -151,6 +149,7 @@ CELERY_ACCEPT_CONTENT = ["json", "msgpack"]
 ASGI_APPLICATION = 'loadify.routing.application'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
--- settings/production.py
+++ settings/production.py
@@ -11,3 +11,11 @@ ALLOWED_HOSTS = [get_env_value('ALLOWED_HOSTS')]
 SECRET_KEY = get_env_value('SECRET_KEY')
 BROKER_URL = get_env_value('BROKER_URL')
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
